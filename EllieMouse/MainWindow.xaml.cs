d5a2a7cb7a8e8ae9be67233579285121e0693d92
Wade Herdsman <wade.herdsman@justice.wa.gov.au>
2022-11-15 16:12:50 +0800

    Commit existing code

---
+++ EllieMouse/MainWindow.xaml.cs
@@ -0,0 +1,233 @@
+﻿using mouseTest;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EllieMouse
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        TimeSpan _timeout = new TimeSpan(10000000);
+
+        IntPtr hWnd;
+        MouseHnd.HookProc _globalLlMouseHookCallback;
+        IntPtr _hGlobalLlMouseHook;
+        Win32.POINT _arrestPoint;
+        bool _arrested = false;
+        bool _dragging = false;
+        DateTime _hold;
+
+        MouseButtonState LeftButtonState;
+        MouseButtonState RightButtonState;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            HideInterface();
+            SetUpHook();
+        }
+
+        private void UpdateMouse()
+        {
+            Win32.POINT p = new Win32.POINT();
+            if (Win32.GetCursorPos(out p))
+            {
+                this.Dispatcher.Invoke((Action)(() => {
+                    WindowInteropHelper windowHwnd = new WindowInteropHelper(this);
+                    hWnd = windowHwnd.Handle;
+                    Win32.SetWindowPos(hWnd, 0, p.X - 75, p.Y - 75, 0, 0, Win32.SWP_NOSIZE);
+                }));
+            }
+        }
+
+        private void ArrestMouse()
+        {
+            _arrested = true;
+            _arrestPoint = new Win32.POINT();
+            Win32.GetCursorPos(out _arrestPoint);
+            ShowInterface();
+        }
+
+        private void FreeMouse()
+        {
+            _arrested = false;
+            MainMouse.IsEnabled = false;
+
+            HideInterface();
+        }
+
+        private void DropMouse()
+        {
+            _dragging = false;
+            Win32.mouse_event((int)(Win32.MouseEventFlags.LEFTUP), 0, 0, 0, 0);
+            FreeMouse();
+        }
+
+        private void ShowInterface()
+        {
+            MainMouse.IsEnabled = true;
+            btnDouble.Visibility = Visibility.Visible;
+            btnTriple.Visibility = Visibility.Visible;
+            btnRight.Visibility = Visibility.Visible;
+            btnDrag.Visibility = Visibility.Visible;
+            btnDouble.IsEnabled = true;
+            btnTriple.IsEnabled = true;
+            btnRight.IsEnabled = true;
+            btnDrag.IsEnabled = true;
+        }
+
+        private void HideInterface()
+        {
+            btnDouble.Visibility = Visibility.Collapsed;
+            btnTriple.Visibility = Visibility.Collapsed;
+            btnRight.Visibility = Visibility.Collapsed;
+            btnDrag.Visibility = Visibility.Collapsed;
+        }
+
+        private void SetUpHook()
+        {
+            // Create an instance of HookProc.
+            _globalLlMouseHookCallback = LowLevelMouseProc;
+
+            _hGlobalLlMouseHook = MouseHnd.NativeMethods.SetWindowsHookEx(
+                MouseHnd.HookType.WH_MOUSE_LL,
+                _globalLlMouseHookCallback,
+                Marshal.GetHINSTANCE(Assembly.GetExecutingAssembly().GetModules()[0]),
+                0);
+
+            if (_hGlobalLlMouseHook == IntPtr.Zero)
+            {
+                //Logger.Fatal("Unable to set global mouse hook");
+                throw new Win32Exception("Unable to set MouseHook");
+            }
+        }
+        private void ClearHook()
+        {
+            //Logger.Debug("Deleting global mouse hook");
+
+            if (_hGlobalLlMouseHook != IntPtr.Zero)
+            {
+                // Unhook the low-level mouse hook
+                if (!MouseHnd.NativeMethods.UnhookWindowsHookEx(_hGlobalLlMouseHook))
+                    throw new Win32Exception("Unable to clear MouseHoo;");
+
+                _hGlobalLlMouseHook = IntPtr.Zero;
+            }
+        }
+
+        public int LowLevelMouseProc(int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            int retInt = MouseHnd.NativeMethods.CallNextHookEx(_hGlobalLlMouseHook, nCode, wParam, lParam);
+
+            if (nCode >= 0 && !_arrested)
+            {
+                var wmMouse = (MouseHnd.MouseMessage)wParam;
+                if (wmMouse == MouseHnd.MouseMessage.WM_MOUSEMOVE)
+                {
+                    UpdateMouse();
+                }
+                else
+                {
+                    if (_dragging)
+                    {
+                        DropMouse();
+                        RightButtonState = MouseButtonState.Released;
+                        return 1;
+                    }
+                    // Get the mouse WM from the wParam parameter
+                    if (wmMouse == MouseHnd.MouseMessage.WM_LBUTTONDOWN && LeftButtonState == MouseButtonState.Released)
+                    {
+                        _hold = DateTime.Now;
+                        LeftButtonState = MouseButtonState.Pressed;
+                    }
+                    if (wmMouse == MouseHnd.MouseMessage.WM_LBUTTONUP && LeftButtonState == MouseButtonState.Pressed)
+                    {
+                        if (DateTime.Now - _hold > _timeout)
+                        {
+                            LeftButtonState = MouseButtonState.Released;
+                            ArrestMouse();
+                            return 1;
+                        }
+                        else
+                        {
+                            LeftButtonState = MouseButtonState.Released;
+                        }
+                    }
+
+                    if (wmMouse == MouseHnd.MouseMessage.WM_RBUTTONDOWN && RightButtonState == MouseButtonState.Released)
+                    {
+                        RightButtonState = MouseButtonState.Pressed;
+                        retInt = 1;
+                    }
+                    if (wmMouse == MouseHnd.MouseMessage.WM_RBUTTONUP && RightButtonState == MouseButtonState.Pressed)
+                    {
+                        RightButtonState = MouseButtonState.Released;
+                        ArrestMouse();
+                        retInt = 1;
+                    }
+                }
+            }
+
+            // Pass the hook information to the next hook procedure in chain
+            return retInt;
+        }
+
+        private void BtnDouble_Click(object sender, RoutedEventArgs e)
+        {
+            Win32.SetCursorPos(_arrestPoint.X, _arrestPoint.Y);
+            Win32.mouse_event((int)(Win32.MouseEventFlags.LEFTDOWN), 0, 0, 0, 0);
+            Win32.mouse_event((int)(Win32.MouseEventFlags.LEFTUP), 0, 0, 0, 0);
+            Win32.mouse_event((int)(Win32.MouseEventFlags.LEFTDOWN), 0, 0, 0, 0);
+            Win32.mouse_event((int)(Win32.MouseEventFlags.LEFTUP), 0, 0, 0, 0);
+            FreeMouse();
+        }
+
+        private void BtnTriple_Click(object sender, RoutedEventArgs e)
+        {
+            Win32.SetCursorPos(_arrestPoint.X, _arrestPoint.Y);
+            Win32.mouse_event((int)(Win32.MouseEventFlags.LEFTDOWN), 0, 0, 0, 0);
+            Win32.mouse_event((int)(Win32.MouseEventFlags.LEFTUP), 0, 0, 0, 0);
+            Win32.mouse_event((int)(Win32.MouseEventFlags.LEFTDOWN), 0, 0, 0, 0);
+            Win32.mouse_event((int)(Win32.MouseEventFlags.LEFTUP), 0, 0, 0, 0);
+            Win32.mouse_event((int)(Win32.MouseEventFlags.LEFTDOWN), 0, 0, 0, 0);
+            Win32.mouse_event((int)(Win32.MouseEventFlags.LEFTUP), 0, 0, 0, 0);
+            FreeMouse();
+        }
+
+        private void BtnRight_Click(object sender, RoutedEventArgs e)
+        {
+            Win32.SetCursorPos(_arrestPoint.X, _arrestPoint.Y);
+            Win32.mouse_event((int)(Win32.MouseEventFlags.RIGHTDOWN), 0, 0, 0, 0);
+            Win32.mouse_event((int)(Win32.MouseEventFlags.RIGHTUP), 0, 0, 0, 0);
+            FreeMouse();
+        }
+
+        private void BtnDrag_Click(object sender, RoutedEventArgs e)
+        {
+            Win32.SetCursorPos(_arrestPoint.X, _arrestPoint.Y);
+            _dragging = true;
+            RightButtonState = MouseButtonState.Pressed;
+            Win32.mouse_event((int)(Win32.MouseEventFlags.LEFTDOWN), 0, 0, 0, 0);
+            FreeMouse();
+        }
+    }
+}
---
+++ EllieMouse/Win32.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mouseTest
+{
+    public class Win32
+    {
+        public const short SWP_NOMOVE = 0X2;
+        public const short SWP_NOSIZE = 1;
+        public const short SWP_NOZORDER = 0X4;
+        public const int SWP_SHOWWINDOW = 0x0040;
+
+        public const uint MOUSEEVENTF_ABSOLUTE = 0x8000;
+        public const uint MOUSEEVENTF_LEFTDOWN = 0x0002;
+        public const uint MOUSEEVENTF_LEFTUP = 0x0004;
+        public const uint MOUSEEVENTF_MIDDLEDOWN = 0x0020;
+        public const uint MOUSEEVENTF_MIDDLEUP = 0x0040;
+        public const uint MOUSEEVENTF_MOVE = 0x0001;
+        public const uint MOUSEEVENTF_RIGHTDOWN = 0x0008;
+        public const uint MOUSEEVENTF_RIGHTUP = 0x0010;
+        public const uint MOUSEEVENTF_XDOWN = 0x0080;
+        public const uint MOUSEEVENTF_XUP = 0x0100;
+        public const uint MOUSEEVENTF_WHEEL = 0x0800;
+        public const uint MOUSEEVENTF_HWHEEL = 0x01000;
+
+        [Flags]
+        public enum MouseEventFlags
+        {
+            LEFTDOWN = 0x00000002,
+            LEFTUP = 0x00000004,
+            MIDDLEDOWN = 0x00000020,
+            MIDDLEUP = 0x00000040,
+            MOVE = 0x00000001,
+            ABSOLUTE = 0x00008000,
+            RIGHTDOWN = 0x00000008,
+            RIGHTUP = 0x00000010
+        }
+
+        [DllImport("User32.Dll")]
+        public static extern long SetCursorPos(int x, int y);
+
+        [DllImport("User32.Dll")]
+        public static extern bool ClientToScreen(IntPtr hWnd, ref POINT point);
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool GetCursorPos(out POINT lpPoint);
+
+        [DllImport("User32.Dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool SetWindowPos(
+          IntPtr hWnd,
+          int hWndInsertAfter,
+          int X,
+          int Y,
+          int cx,
+          int cy,
+          int Flags
+          );
+
+        [DllImport("user32.dll")]
+        public static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint dwData, int dwExtraInfo);
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct POINT
+        {
+            public int X;
+            public int Y;
+            public POINT(int x, int y)
+            {
+                this.X = x;
+                this.Y = y;
+            }
+        }
+
+
+    }
+}
